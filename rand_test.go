05645bbee97e45f9283c43f24220f85678cb1142
salrashid123 <salrashid123@gmail.com>
2024-05-06 09:30:58 -0400

    add tests; github workflow

---
+++ rand_test.go
@@ -0,0 +1,46 @@
+package tpmrand
+
+import (
+	"crypto/rsa"
+	"testing"
+
+	"github.com/google/go-tpm-tools/simulator"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRandBytes(t *testing.T) {
+	tpmDevice, err := simulator.Get()
+	require.NoError(t, err)
+	defer tpmDevice.Close()
+
+	randomBytes := make([]byte, 32)
+	r, err := NewTPMRand(&Reader{
+		TpmDevice: tpmDevice,
+		//Scheme:    backoff.NewConstantBackOff(time.Millisecond * 10),
+	})
+	require.NoError(t, err)
+
+	_, err = r.Read(randomBytes)
+	require.NoError(t, err)
+
+	require.Equal(t, len(randomBytes), 32)
+}
+
+func TestRSAKey(t *testing.T) {
+	tpmDevice, err := simulator.Get()
+	require.NoError(t, err)
+	defer tpmDevice.Close()
+
+	r, err := NewTPMRand(&Reader{
+		TpmDevice: tpmDevice,
+		//Scheme:    backoff.NewConstantBackOff(time.Millisecond * 10),
+	})
+	require.NoError(t, err)
+
+	// /// RSA keygen
+	privkey, err := rsa.GenerateKey(r, 2048)
+	require.NoError(t, err)
+
+	rsaPubKey := privkey.PublicKey
+	require.Equal(t, 2048, rsaPubKey.Size()*8)
+}
